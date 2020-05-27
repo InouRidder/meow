8290ef584f65c2d5f6c353c55ef2c118651bd3dc
Inou Ridder <inouridder@gmail.com>
2020-05-27 17:36:46 +0200

    ft: serve up cats index

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Cat;
 
 class CatController extends Controller
 {
-    //
+    public function index() {
+        $cats = Cat::all();
+
+        return $cats;
+    }
 }
